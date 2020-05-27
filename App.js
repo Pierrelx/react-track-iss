b7294b6c7c547e05e6e78713b910b62aac5c8017
PierreLx <leroux.pierre.76@gmail.com>
2020-05-27 19:04:52 +0200

    Init

--- App.js
+++ App.js
@@ -1,11 +1,43 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import MapView, {Marker} from 'react-native-maps';
 
 export default function App() {
+  const [position, setPosition] = useState({
+    latitude: 49.4431,
+    longitude: 1.0992,
+    latitudeDelta: 0.05,
+    longitudeDelta: 0.05,
+  })
+
+  async function getPosition(){
+    try {
+      let response = await fetch(
+        'http://api.open-notify.org/iss-now.json'
+      );
+      let json = await response.json();
+      setPosition({
+        latitude: parseFloat(json.iss_position.latitude),
+        longitude: parseFloat(json.iss_position.longitude),
+        latitudeDelta: 1,
+        longitudeDelta: 1,
+      })
+    } catch(error){
+      console.log(error)
+    }
+  }
+  getPosition()
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+      <MapView
+        style={{flex: 1}}
+        region={position}
+        >
+          <Marker coordinate={position} title={"ISS"}>
+            <Image source={require("./assets/img/iss.png")} style={{width: 60, height: 60}}/>
+          </Marker>
+        </MapView>
+      
   );
 }
 
